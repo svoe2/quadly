d816d715c47d5910bc6fab265a27225f7df6c377
svoe2 <129616332+svoe2@users.noreply.github.com>
2024-08-07 14:11:22 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-if [ ! -f plugins/Hibernate.jar ]; then
-    curl -Lo plugins/Hibernate.jar https://raw.githubusercontent.com/svoe2/quadly/main/Hibernate.jar
-fi
+if [ ! -f plugins/Hibernate.jar ]; then curl -Lo plugins/Hibernate.jar https://raw.githubusercontent.com/svoe2/quadly/main/Hibernate.jar fi
 
 java -Xms128M -XX:MaxRAMPercentage=90 -Dterminal.jline=false -Dterminal.ansi=true -jar $SERVER_JARFILE
